2d55fd6aca1bb1a74106658a9159a24cdb69475a
Kishore_M <133372253+KishoreMuruganantham@users.noreply.github.com>
2024-12-24 20:39:48 +0530

    Update config.ts

--- Mimir_AI Frontend/Mimir AI/Mimir AI website/src/firebase/config.ts
+++ Mimir_AI Frontend/Mimir AI/Mimir AI website/src/firebase/config.ts
@@ -6,10 +6,10 @@ const firebaseConfig = {
   apiKey: "",
   authDomain: "",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  databaseURL:"https://example-fastapi-f5a95-default-rtdb.firebaseio.com/",
-  storageBucket: "example-fastapi-f5a95.appspot.com",
-  messagingSenderId: "1024331035169",
-  appId: "1:1024331035169:web:42773da2833da4e5a92960"
+  databaseURL:"",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
