7f91f816e7f978543cf20ef402d5e946fc1d803c
Kishore_M <133372253+KishoreMuruganantham@users.noreply.github.com>
2024-12-24 20:39:22 +0530

    Update config.ts

--- Mimir_AI Frontend/Mimir AI/Mimir AI website/src/firebase/config.ts
+++ Mimir_AI Frontend/Mimir AI/Mimir AI website/src/firebase/config.ts
@@ -3,8 +3,8 @@ import { getStorage } from 'firebase/storage';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXyeap4l3_gNoTFR4-YX3MJH8PE-9Qn1w",
-  authDomain: "example-fastapi-f5a95.firebaseapp.com",
+  apiKey: "",
+  authDomain: "",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   databaseURL:"https://example-fastapi-f5a95-default-rtdb.firebaseio.com/",
   storageBucket: "example-fastapi-f5a95.appspot.com",
